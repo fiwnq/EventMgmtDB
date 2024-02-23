6c3fbe18ec485d2293291494b8fb036e0993d8c5
test <test@example.com>
2024-02-23 11:51:42 -0600

    added inserts file

---
+++ inserts.sql
@@ -0,0 +1,11 @@
+INSERT INTO Events (Title, DateTime, Description)
+VALUES ('Lipscomb TED Talk', '2024-02-13', 'A short TED talk by an esteemed professor from Lipscomb University');
+
+INSERT INTO Events (Title, DateTime, Description)
+VALUES ('Importance of Motivational Quotes', '2024-07-07', 'A comprehensive talk from a life coach on his experience with motivational quotes affecting both his life and the lives of many others.');
+
+INSERT INTO Events (Title, DateTime, Description)
+VALUES ('Cybersecurity Showcase', '2024-11-27', 'A knowledgeable IT professor will showcase popular hacking attack and how you can be safe on the internet.');
+
+INSERT INTO Speakers (Name, Bio, ContactInfo)
+VALUES (

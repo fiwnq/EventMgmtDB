7882ee0dc1e448fb641bc89d5787dc4ad9ec2838
test <test@example.com>
2024-04-05 11:42:40 -0500

    added attendee and organization schemas

--- models/schemas.py
+++ models/schemas.py
@@ -19,3 +19,17 @@ class ActorInfo(db.Model):
     first_name = db.Column(db.String(20), unique=False, nullable=False)
     last_name = db.Column(db.String(20), unique=False, nullable=False)
     film_info = db.Column(db.String(255), nullable=True)
+
+class Organization(db.Model):
+
+    org_id = db.Column(db.Integer, primary_key=True)
+    org_name = db.Column(db.String(50), unique=True, nullable=True)
+    org_desc = db.Column(db.String(300), unique=False, nullable=True)
+
+
+class Attendee(db.Model):
+
+    att_id = db.Column(db.Integer, primary_key=True)
+    att_name = db.Column(db.String(50), unique=True, nullable=True)
+    att_email = db.Column(db.String(100), unique=False, nullable=True)
+    org_id = db.Column(db.Integer, unique=False, nullable=True) # foreign key, how do i implement this?

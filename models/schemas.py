d5d5d96d7227cfd57a9def90afba95e369f7999e
test <test@example.com>
2024-04-07 16:36:39 -0500

    cleaned up

--- models/schemas.py
+++ models/schemas.py
@@ -12,13 +12,15 @@ class Actor(db.Model):
  
     def __repr__(self):
         return f"Name : {self.first_name}, Age: {self.age}"
-    
+
+
 class ActorInfo(db.Model):
 
     actor_id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(20), unique=False, nullable=False)
     last_name = db.Column(db.String(20), unique=False, nullable=False)
     film_info = db.Column(db.String(255), nullable=True)
+
 
 class Organization(db.Model):
 
@@ -32,8 +34,8 @@ class Attendee(db.Model):
     att_id = db.Column(db.Integer, primary_key=True)
     att_name = db.Column(db.String(50), unique=True, nullable=False)
     att_email = db.Column(db.String(100), unique=False, nullable=True)
-<<<<<<< HEAD
     org_id = db.Column(db.Integer, ForeignKey(Organization.org_id), unique=False, nullable=False) # foreign key, how do i implement this?
+
 
 class Speaker(db.Model):
 
@@ -42,12 +44,10 @@ class Speaker(db.Model):
     speaker_bio = db.Column(db.String(1000), unique=False, nullable=True)
     speaker_info = db.Column(db.String(100), unique=False, nullable=True)
 
-=======
-    org_id = db.Column(db.Integer, unique=False, nullable=True) # foreign key, how do i implement this?
 
 class Event(db.Model):
+    
     event_id = db.Column(db.Integer, primary_key = True)
     event_name = db.Column(db.String(100), unique = True, nullable = False)
     event_datetime = db.Column(db.DateTime, unique = True, nullable = False)
     event_desc = db.Column(db.String(1000), unique = False, nullable = True)
->>>>>>> 361e42ee3cdf15c3ad2a93e921ec7d3a14ad7a46

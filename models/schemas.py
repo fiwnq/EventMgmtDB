766f982a40a27db986e27388ded4afd3a7320495
test <test@example.com>
2024-04-17 17:11:51 -0500

    changed column names across all tables to match

--- models/schemas.py
+++ models/schemas.py
@@ -14,6 +14,15 @@ class Actor(db.Model):
         return f"Name : {self.first_name}, Age: {self.age}"
 
 
+class Speakers(db.Model):
+
+    speaker_id = db.Column(db.Integer, primary_key = True)
+    speaker_name = db.Column(db.String(50), unique = True, nullable = False)
+    speaker_bio = db.Column(db.String(1000), unique = False, nullable = True)
+    speaker_info = db.Column(db.String(100), unique = False, nullable = True)
+    last_update = db.Column(DateTime(timezone=True), nullable=True, onupdate=func.now())
+
+
 class Events(db.Model):
     
     event_id = db.Column(db.Integer, primary_key = True)
@@ -28,7 +37,7 @@ class Venues(db.Model):
     venue_id = db.Column(db.Integer, primary_key = True)
     venue_name = db.Column(db.String(100), unique = True, nullable = False)
     venue_location = db.Column(db.String(50), unique = False, nullable = False)
-    venue_capacity = db.Column(db.Integer, unique = False, nullable = False)
+    venue_capacity = db.Column(db.Integer, unique = False, nullable = True)
     venue_contact = db.Column(db.String(100), unique = False, nullable = True)
     last_update = db.Column(DateTime(timezone=True), nullable=True, onupdate=func.now())
 
@@ -63,27 +72,11 @@ class Bookings(db.Model):
 class Catering(db.Model):
 
     catering_id = db.Column(db.Integer, primary_key = True)
-    catering_business = db.Column(db.String(100), unique = True, nullable = False)
-    catering_type = db.Column(db.String(50), unique = False, nullable = False)
-    last_update = db.Column(DateTime(timezone=True), nullable=True, onupdate=func.now())
-
-
-class CateringService(db.Model):
-
-    cs_id = db.Column(db.Integer, primary_key = True)
-    cs_catering_id = db.Column(db.Integer, ForeignKey(Catering.catering_id), unique = False, nullable = False)
-    cs_event_id = db.Column(db.Integer, ForeignKey(Events.event_id), unique = False, nullable = False)
-    cs_servings = db.Column(db.Integer, unique = False, nullable = False)
-    cs_time = db.Column(db.Time, unique = False, nullable = False)
-    last_update = db.Column(DateTime(timezone=True), nullable=True, onupdate=func.now())
-
-
-class Speakers(db.Model):
-
-    speaker_id = db.Column(db.Integer, primary_key = True)
-    speaker_name = db.Column(db.String(50), unique = True, nullable = False)
-    speaker_bio = db.Column(db.String(1000), unique = False, nullable = True)
-    speaker_info = db.Column(db.String(100), unique = False, nullable = True)
+    catering_business = db.Column(db.String(100), unique = True, nullable = True)
+    catering_type = db.Column(db.String(50), unique = False, nullable = True)
+    catering_event_id = db.Column(db.Integer, ForeignKey(Events.event_id), unique = False, nullable = False)
+    catering_servings = db.Column(db.Integer, unique = False, nullable = True)
+    catering_time = db.Column(db.Time, unique = False, nullable = True)
     last_update = db.Column(DateTime(timezone=True), nullable=True, onupdate=func.now())
 
 

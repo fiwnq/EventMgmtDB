58692987d679a5c466929e8d745388a19761563c
test <test@example.com>
2024-04-21 15:11:24 -0500

    removed actor, fixed dropdowns

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from core import app
-from flask import redirect, request 
+from flask import redirect, request
 from flask.templating import render_template
-from models import Actor, Attendee, Booking, Catering, Event, Organization, Speaker, Station, Venue
+from models import Attendee, Booking, Catering, Event, Organization, Speaker, Station, Venue
 
 @app.route('/hello')
 def hello():
@@ -10,53 +10,6 @@ def hello():
 @app.route('/')
 def home():
 	return render_template('home.html')
-
-# -------------------------- ACTORS --------------------------------
-
-# unused?
-@app.route('/get_actors', methods=['GET','POST']) 
-def get_results(): 
-    actors = Actor.get_actors()  
-    return actors 
-
-# APP ROUTE TO RENDER INDEX PAGE WITH LIST OF ACTORS
-@app.route('/actor_index')
-def actor_index():
-	# Query all data and then pass it to the template
-	actors = Actor.get_actors()
-	return render_template('actor_index.html', actors=actors)
-
-# APP ROUTE TO RENDER FORM TO ADD ACTOR DATA
-@app.route('/add_actor_data')
-def add_actor_data():
-	return render_template('add_actor.html')
-
-# APP ROUTE TO CALL FUNCTION TO ADD ACTOR
-@app.route('/add_actor', methods=["POST"])
-def add_actor():
-	
-	# In this function we will input data from the 
-	# form page and store it in our database.
-	# Remember that inside the get the name should
-	# exactly be the same as that in the html
-	# input fields
-	first_name = request.form.get("first_name")
-	last_name = request.form.get("last_name")
-
-	# call model function that will store data as a row in our datatable
-	if first_name != '' and last_name != '':
-		Actor.add_actor(first_name, last_name)
-		return redirect('/')
-	else:
-		return redirect('/')
-
-# APP ROUTE TO CALL FUNCTION TO DELETE ACTOR
-@app.route('/delete_actor/<int:id>')
-def delete_actor(id):
-	# Deletes the data on the basis of unique id and 
-	# redirects to home page
-	Actor.delete_actor(id)
-	return redirect('/')
 
 # -------------------------- EVENTS --------------------------------
 
